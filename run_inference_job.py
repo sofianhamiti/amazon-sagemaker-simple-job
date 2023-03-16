862db6013539298a88cf63dbf1a71835730847e5
EC2 Default User <ec2-user@ip-172-16-54-146.eu-west-1.compute.internal>
2023-03-16 16:58:55 +0000

    add container uri placeholder

--- run_inference_job.py
+++ run_inference_job.py
@@ -3,12 +3,10 @@ from sagemaker.estimator import Estimator
 
 if __name__ == "__main__":
     # PARAMETERS
-    container_uri = (
-        "366243680492.dkr.ecr.eu-west-1.amazonaws.com/custom-training:latest"
-    )
+    container_uri = "<ADD ECR IMAGE URI HERE>"
     instance_type = "ml.g5.2xlarge"
 
-    # GET IAM ROLE FROM ENVIRONMENT
+    # GET IAM ROLE FROM CURRENT ENVIRONMENT
     iam_role = sagemaker.get_execution_role()
 
     # SETUP JOB
@@ -17,7 +15,7 @@ if __name__ == "__main__":
         role=iam_role,
         instance_count=1,
         instance_type=instance_type,
-        # if needed, you can pass parameters as environment variables
+        # if needed, you can use environment variables to pass parameters to container
         environment={"test": "value"},
     )
 
